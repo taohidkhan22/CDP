1c980a022530d19934f6240e6e9951f18093031d
taohidkhan22 <taohid130@gmail.com>
2024-02-18 16:41:56 +0600

    both contact and about are dynamically worked

--- sript.js
+++ sript.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-        const navLinks = document.querySelectorAll('.nav_link');
-        
-        navLinks.forEach(link => {
-            link.addEventListener('click', function(e) {
-                e.preventDefault();
-                const sectionId = this.getAttribute('data-section');
-                const section = document.getElementById(sectionId);
+    const navLinks = document.querySelectorAll('.nav_link');
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const sectionId = this.getAttribute('data-section');
+            const section = document.getElementById(sectionId);
+            if (sectionId === 'contact') { 
+                section.style.display = 'block'; 
+                section.scrollIntoView({ behavior: 'smooth' }); n
+            } else {
+                
                 if (section) {
                     section.scrollIntoView({ behavior: 'smooth' });
                 }
-            });
+            }
         });
     });
-
+});
