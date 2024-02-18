694447fc0ca6106d2311d9fe3c8e00a6f05b1f5b
taohidkhan22 <taohid130@gmail.com>
2024-02-18 17:25:35 +0600

    Scroll down option added

--- sript.js
+++ sript.js
@@ -18,3 +18,24 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+}
+function scrollToTop() {
+ 
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
