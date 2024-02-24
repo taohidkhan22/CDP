f400da8551cfb4a7bc0680a11a8e77a4c97a3288
taohidkhan22 <taohid130@gmail.com>
2024-02-24 19:06:48 +0600

    json to table working (manually)

--- json to table/script.js
+++ json to table/script.js
@@ -1,23 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var myarray = [
+        { 'name': 'Taohid', 'id': '25', 'subject': 'DSD', 'marks': '25' },
+        { 'name': 'Taohid', 'id': '25', 'subject': 'DSD', 'marks': '25' },
+        { 'name': 'Taohid', 'id': '25', 'subject': 'DSD', 'marks': '25' },
+    ];
 
-var myarray = [
-    { 'name': 'Taohid', 'id': '25', 'subject': 'DSD', 'marks': '25' },
-    { 'name': 'Taohid', 'id': '25', 'subject': 'DSD', 'marks': '25' },
-    { 'name': 'Taohid', 'id': '25', 'subject': 'DSD', 'marks': '25' },
-];
+    buildtable(myarray);
 
-buildtable(myarray);
-
-function buildtable(data) {
-    var tableBody = document.getElementById('myTable');
-    var rows = '';
-    for (var i = 0; i < data.length; i++) {
-        var row = `<tr> 
-            <td>${data[i].name}</td>
-            <td>${data[i].id}</td>
-            <td>${data[i].subject}</td>
-            <td>${data[i].marks}</td>
-        </tr>`;
-        rows += row; 
+    function buildtable(data) {
+        var tableBody = document.getElementById('myTable');
+        if (tableBody) {
+            var rows = '';
+            for (var i = 0; i < data.length; i++) {
+                var row = `<tr> 
+                    <td>${data[i].name}</td>
+                    <td>${data[i].id}</td>
+                    <td>${data[i].subject}</td>
+                    <td>${data[i].marks}</td>
+                </tr>`;
+                rows += row; 
+            }
+            tableBody.innerHTML = rows;
+        } else {
+            console.error("Element with ID 'myTable' not found.");
+        }
     }
-    tableBody.innerHTML = rows;
-}
+});
