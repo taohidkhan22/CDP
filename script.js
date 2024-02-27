51dcc75aac766e4a9e62d64c0393cc8564763185
taohidkhan22 <taohid130@gmail.com>
2024-02-27 20:42:07 +0600

    srcipt change

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ function myFunction() {
     tr = table.getElementsByTagName("tr");
 
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[2];
+        td = tr[i].getElementsByTagName("td")[1];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
